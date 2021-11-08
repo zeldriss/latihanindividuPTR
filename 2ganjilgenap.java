5d37b4324a6c233398f7ddf0ae2173e213b0c1e9
zeldriss <31651756+zeldriss@users.noreply.github.com>
2021-11-08 14:12:51 +0700

    Create 2ganjilgenap.java

---
+++ 2ganjilgenap.java
@@ -0,0 +1,20 @@
+package latihanindividuptr;
+
+import java.util.Scanner;
+
+public class ganjilgenap {
+     int angka;
+    public static void main(String args[])
+  {
+    int a;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Masukkan sebuah bilangan : ");
+    a = scan.nextInt();
+    if (a % 2 == 0) {
+      System.out.print("Bilangan " + a + " adalah genap.\n");
+    } else {
+      System.out.print("Bilangan " + a + " adalah ganjil.\n");
+    }
+  }
+}
+
